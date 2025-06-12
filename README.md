84d5e4a7345ece0dc0024a14c0fc61b439df7079
codechirag <chiraggusani94@gmail.com>
2025-06-13 04:46:10 +0530

    page completed with responsive and animations added

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# stem
-stem project for praella
+# my-website/README.md
+
+# My Website
+
+This is a standard HTML project structure for a simple website.
+
+## Project Structure
+
+- **src/**: Contains all the source files for the website.
+  - **assets/**: Contains static assets like CSS and JavaScript files.
+    - **css/**: Contains stylesheets.
+    - **js/**: Contains JavaScript files.
+  - **pages/**: Contains HTML pages for the website.
+  - **index.html**: The main entry point of the website.
+
+## Getting Started
+
+To get started with this project, simply open the `index.html` file in your web browser.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/assets/js/main.js
@@ -0,0 +1,124 @@
+//  First Tab Slider initialization
+var swiper = new Swiper(".timelessSwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    breakpoints: {
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 30,
+        },
+    },
+    scrollbar: {
+        el: ".timeless-swiper-scrollbar",
+        draggable: true
+    },
+});
+
+//  Second Tab Slider initialization
+var swiper = new Swiper(".trendingSwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    breakpoints: {
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 30,
+        },
+    },
+    scrollbar: {
+        el: ".trending-swiper-scrollbar",
+        draggable: true
+    },
+});
+
+//  Third Tab Slider initialization
+var swiper = new Swiper(".kidsSwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    breakpoints: {
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 30,
+        },
+    },
+    scrollbar: {
+        el: ".kids-swiper-scrollbar",
+        draggable: true
+    },
+});
+
+
+// GSAP Animation code
+
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const elementX = (mouseX / window.innerWidth) * 50;
+    const elementY = (mouseY / window.innerHeight) * 50;
+
+    // Animate the element using GSAP
+    gsap.to(".blue-shape,.orange-shape,.green-shape", {
+        rotateX: -elementX,
+        rotateY: -elementY,
+        duration: 0.5,
+        ease: "power2.out"
+    });
+});
+
+// Text animation 
+
+const texts = document.querySelectorAll('.text-animation');
+texts.forEach(text => {
+    const splitText = new SplitType(text, { types: 'words, chars' });
+
+    gsap.from(splitText.chars, {
+        duration: 0.5,
+        y: 100,
+        opacity:0.2,
+        stagger: 0.02,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: text,
+            start: 'top 80%',
+            // scrub: 1, // optional if we need smooth scrubbing on scroll animation
+            // toggleActions: 'play none none reverse'
+        }
+    });
+});
+
+// box fade up animation
+gsap.from('.box', {
+    duration: 0.5,
+    opacity: 0,
+    y: 100,
+    stagger: 0.2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".third-section",
+        start: 'top 80%',
+    }
+});
